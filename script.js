81c98ed7a8bc39ad422a8babea0f103e72599d20
l-hso <lhsoliveira2008@hotmail.com>
2024-09-13 22:41:55 -0300

    input correto finaliza o jogo

--- script.js
+++ script.js
@@ -1,72 +1,73 @@
-window.addEventListener("load", ()=>{
-    const PALAVRAS_PERMITIDAS = ["acido", "chuva", "metil", "butil"];
+window.addEventListener("load", () => {
+  const PALAVRAS_PERMITIDAS = ["acido", "chuva", "metil", "butil"];
 
-    let palavra_da_vez = PALAVRAS_PERMITIDAS[Math.ceil(Math.random() * (PALAVRAS_PERMITIDAS.length-1))]
+  let palavra_da_vez =
+    PALAVRAS_PERMITIDAS[
+      Math.ceil(Math.random() * (PALAVRAS_PERMITIDAS.length - 1))
+    ];
 
-    const inputs_linhas = document.querySelectorAll("#inputs_container .linhas");
-    
-    const REGEX_LETRAS = /\b[a-zA-Z]\b/;
-   
-    let linha_atual = 0;
-    
-    window.addEventListener("keydown", (e)=>{
-        if(e.key == "Enter"){    
-            alert(palavra_da_vez);
-            let palavra = Array.from(inputs_linhas[linha_atual].children).reduce((a,b)=> a + b.value, "").toLowerCase();
-    
-            if(palavra.length < 5){
-                document.querySelector("#mensagem_de_erro").textContent = "Só palavras com 5 letras";
+  const inputs_linhas = document.querySelectorAll("#inputs_container .linhas");
 
-                document.querySelector("#mensagem_de_erro").style.display = "block";
-            } else { 
+  const REGEX_LETRAS = /\b[a-zA-Z]\b/;
 
-                if (!PALAVRAS_PERMITIDAS.includes(palavra)){
-                    document.querySelector("#mensagem_de_erro").textContent = "Essa palavra não é aceita";
+  let linha_atual = 0;
 
-                    document.querySelector("#mensagem_de_erro").style.display = "block";
+  window.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+      alert(palavra_da_vez);
+      let palavra = Array.from(inputs_linhas[linha_atual].children)
+        .reduce((a, b) => a + b.value, "")
+        .toLowerCase();
 
-                } else {
-                    Array.from(inputs_linhas[linha_atual].children).forEach((el, ind)=>{
-                        if(palavra_da_vez.includes(el.value)){
-                            if(palavra_da_vez.indexOf(el.value) == ind){
-                                el.classList = "input_acerto";
-                            } else {
-                                el.classList = "input_acertoparcial";
-                            }
-                        } else {
-                            el.classList = "input_erro";
-                        }
+      if (palavra.length < 5) {
+        document.querySelector("#mensagem_de_erro").textContent =
+          "Só palavras com 5 letras";
 
-                        el.setAttribute("disabled", "true")
-                    })
+        document.querySelector("#mensagem_de_erro").style.display = "block";
+      } else {
+        if (!PALAVRAS_PERMITIDAS.includes(palavra)) {
+          document.querySelector("#mensagem_de_erro").textContent =
+            "Essa palavra não é aceita";
 
-                    linha_atual++;
-
-                    Array.from(inputs_linhas[linha_atual].children).forEach((el)=> el.removeAttribute("disabled"))
-                }
+          document.querySelector("#mensagem_de_erro").style.display = "block";
+        } else {
+          // Se o input for compativel muda as cores dos blocos
+          Array.from(inputs_linhas[linha_atual].children).forEach((el, ind) => {
+            if (palavra_da_vez.includes(el.value)) {
+              if (palavra_da_vez.indexOf(el.value) == ind) {
+                el.classList = "input_acerto";
+              } else {
+                el.classList = "input_acertoparcial";
+              }
+            } else {
+              el.classList = "input_erro";
             }
 
-        } else {
-            document.querySelector("#mensagem_de_erro").style.display = "none";
-            
+            el.setAttribute("disabled", "true");
+          });
+
+          if (palavra != palavra_da_vez) {
+            linha_atual++;
+            Array.from(inputs_linhas[linha_atual].children).forEach((el) =>
+              el.removeAttribute("disabled")
+            );
+          }
         }
-    })
-    
-    
-    Array.from(inputs_linhas[linha_atual].children).forEach((el)=>{
-                
-        el.addEventListener("keydown", (e)=>{
-        
-             if(REGEX_LETRAS.test(e.key)){
-                el.value = e.key;
+      }
+    } else {
+      document.querySelector("#mensagem_de_erro").style.display = "none";
+    }
+  });
 
-             } else if(e.key == "Backspace"){
-                el.value = "";
-             }
+  // Keyboard melhor
 
-             
-        })
-        
-    })
-})
-
+  Array.from(inputs_linhas[linha_atual].children).forEach((el) => {
+    el.addEventListener("keydown", (e) => {
+      if (REGEX_LETRAS.test(e.key)) {
+        el.value = e.key;
+      } else if (e.key == "Backspace") {
+        el.value = "";
+      }
+    });
+  });
+});
