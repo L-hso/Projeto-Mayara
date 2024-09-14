b6e0381fdf7a097938fab40a95cd70ef4d711c8f
l-hso <lhsoliveira2008@hotmail.com>
2024-09-14 15:17:46 -0300

    Falta configs, dicas e palavras a mais

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-  const PALAVRAS_PERMITIDAS = ["acido", "chuva", "metil", "butil"];
+  const PALAVRAS_PERMITIDAS = ["acida", "chuva", "metil", "butil", "octil"];
 
   let palavra_da_vez =
     PALAVRAS_PERMITIDAS[
@@ -12,62 +12,97 @@ window.addEventListener("load", () => {
 
   let linha_atual = 0;
 
+  // Keyboard melhor 1 ocorrencia
+  Array.from(inputs_linhas[linha_atual].children).forEach((el) => {
+
+    el.addEventListener("keydown", (e) => {
+      if (REGEX_LETRAS.test(e.key)) {
+        el.value = e.key.toLowerCase();
+      } else if (e.key == "Backspace") {
+        el.value = "";
+      }
+    });
+  });
+  //----------------------------------
+
   window.addEventListener("keydown", (e) => {
+    //Verifica se o jogador clicou enter
     if (e.key == "Enter") {
       alert(palavra_da_vez);
       let palavra = Array.from(inputs_linhas[linha_atual].children)
         .reduce((a, b) => a + b.value, "")
         .toLowerCase();
 
+
+      //Verifica se a palavra tem 5 letras 
       if (palavra.length < 5) {
         document.querySelector("#mensagem_de_erro").textContent =
           "Só palavras com 5 letras";
 
         document.querySelector("#mensagem_de_erro").style.display = "block";
+
+        //Verifica se a palavra é permitida
+      } else if (!PALAVRAS_PERMITIDAS.includes(palavra)) {
+        document.querySelector("#mensagem_de_erro").textContent =
+          "Essa palavra não é aceita";
+
+        document.querySelector("#mensagem_de_erro").style.display = "block";
+
+
+      // Se o input for compativel muda as cores dos inputs
       } else {
-        if (!PALAVRAS_PERMITIDAS.includes(palavra)) {
-          document.querySelector("#mensagem_de_erro").textContent =
-            "Essa palavra não é aceita";
+        let p = palavra_da_vez.split("");
 
-          document.querySelector("#mensagem_de_erro").style.display = "block";
-        } else {
-          // Se o input for compativel muda as cores dos blocos
-          Array.from(inputs_linhas[linha_atual].children).forEach((el, ind) => {
-            if (palavra_da_vez.includes(el.value)) {
-              if (palavra_da_vez.indexOf(el.value) == ind) {
-                el.classList = "input_acerto";
-              } else {
-                el.classList = "input_acertoparcial";
-              }
-            } else {
-              el.classList = "input_erro";
-            }
+        Array.from(inputs_linhas[linha_atual].children).forEach((input, input_ind) => {
 
-            el.setAttribute("disabled", "true");
-          });
-
-          if (palavra != palavra_da_vez) {
-            linha_atual++;
-            Array.from(inputs_linhas[linha_atual].children).forEach((el) =>
-              el.removeAttribute("disabled")
-            );
+          if (p.some((letter, index) => input.value == letter && index == input_ind)) {
+            input.classList = "input_acerto";
+          } else if (p.some((letter, _) => input.value == letter)) {
+            input.classList = "input_acertoparcial";
+          } else {
+            input.classList = "input_erro";
           }
+
+          input.setAttribute("disabled", "true");
+
+        });
+        linha_atual++;
+        // Libera o próximo input se a palavra estiver incorreta
+        if (palavra != palavra_da_vez && linha_atual != 4) {
+          
+          
+
+          Array.from(inputs_linhas[linha_atual].children).forEach((el) =>
+            el.removeAttribute("disabled")
+          );
+
+          Array.from(inputs_linhas[linha_atual].children).forEach((el) => {
+            el.addEventListener("keydown", (i) => {
+              if (REGEX_LETRAS.test(i.key)) {
+                el.value = i.key.toLowerCase();
+              }
+
+              if (i.key == "Backspace") {
+                el.value = "";
+              }
+
+              document.querySelector("#mensagem_de_erro").style.display = "none";
+            });
+          });
+          console.log(linha_atual)
+
+        } else if (palavra == palavra_da_vez) {
+          document.querySelector("#info_final #painel_de_reset h1").textContent = "Você ganhou!"
+          document.querySelector("#info_final").style.display = "flex";
+        } else {
+
+          document.querySelector("#info_final #painel_de_reset h1").textContent = "Você perdeu!"
+          document.querySelector("#info_final").style.display = "flex";
         }
+
+
       }
-    } else {
-      document.querySelector("#mensagem_de_erro").style.display = "none";
     }
   });
-
-  // Keyboard melhor
-
-  Array.from(inputs_linhas[linha_atual].children).forEach((el) => {
-    el.addEventListener("keydown", (e) => {
-      if (REGEX_LETRAS.test(e.key)) {
-        el.value = e.key;
-      } else if (e.key == "Backspace") {
-        el.value = "";
-      }
-    });
-  });
 });
+
